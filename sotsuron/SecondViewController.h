e2a2f7be41d646bb64780b8e19ee51288f7402a8
AstrayBI <astraybi@yahoo.co.jp>
2014-05-20 13:01:14 +0900

    Initial Commit

---
+++ sotsuron/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  sotsuron
+//
+//  Created by Astray Bi on 2014/05/20.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
